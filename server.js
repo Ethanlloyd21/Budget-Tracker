543a858debd821062ac1cac0a1a831d665def57a
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-04-03 06:20:01 -0700

    update

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require(`express`);
+const mongoose = require(`mongoose`);
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(`public`));
+
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/budget`, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+// routes
+app.use(require(`./routes/api.js`));
+
+app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
